99c27c97dbf5dc402ac76f14a82a93869a3316d3
Ruhanjali <anjalitomarbk4@gmail.com>
2022-05-07 12:45:37 +0530

    eventapi

---
+++ route/eventCategoryRoute.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const multer = require("multer");
+const eventCategoryController = require("../controller/eventCategoryController");
+const { body } = require('express-validator');
+
+var storages = multer.diskStorage({
+    destination: "public/images",
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + "-" + file.originalname);
+    },
+  });
+  
+var upload = multer({ storage: storages });
+
+router.post("/add" , upload.single("image") ,
+body("name").notEmpty(),
+body("image").notEmpty(),
+eventCategoryController.add);
+
+router.get("/view",eventCategoryController.view)
+
+router.post("/update",upload.single("image"),
+body("name").notEmpty(),
+body("image").notEmpty(),
+body("oldImage").notEmpty(),
+eventCategoryController.update);
+
+router.delete("/delete/:id", eventCategoryController.deletebyid);
+
+router.post("/view-product-categoryId",body("id").notEmpty(), eventCategoryController.viewEventCategoryById);
+
+module.exports = router;
+ 
